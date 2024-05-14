3c48845e59e09c088952779639d5d0f1f44a438f
mortolik <mortolik@mail.ru>
2024-05-15 00:22:33 +0300

    Add TTable class

--- TTable/TRecord.h
+++ TTable/TRecord.h
@@ -9,30 +9,72 @@ typedef string TValue;
 
 struct TRecord
 {
-	TKey key;
-	TValue val;
+	TKey key;     //Ключ
+	TValue value; //Запись
+
+	TRecord()
+	{
+		key = 0;
+		value = "";
+	}
+
+	TRecord(TKey key, TValue value)
+	{
+		this->key = key;
+		this->value = value;
+	}
+
+	TRecord& operator=(const TRecord& other)
+	{
+		key = other.key;
+		value = other.value;
+		return *this;
+	}
+
+	void output()
+	{
+		std::cout << "( " << key << "  " << value << " )" << std::endl;
+	}
+	bool operator == (TRecord other)
+	{
+		if (this->value == other.value)
+			return true;
+		else
+			return false;
+	}
+	bool operator != (const TRecord& other)
+	{
+		return !(*this == other);
+	}
 };
+
 
 class TTable
 {
 protected:
-	int DataCount, eff;
+	int dataCount;   //Количество записей в таблице на данный момент
+	int efficiency;
 
 public:
-	TTable() { DataCount = 0; eff = 0; };
-	virtual ~TTable();
-	int GetDataCount() { return DataCount; }
-	int GetEff() { return eff; }
-	void ClearEff() { eff = 0; }
-	bool IsEmpty() { return DataCount == 0; }
-	virtual bool IsFull() = 0;
-	virtual bool Find(TKey key) = 0;
-	virtual bool InsRecord(TRecord rec) = 0;
-	virtual void DelRecord(TKey key) = 0;
-	virtual void Reset() = 0;
+	TTable() { dataCount = 0; efficiency = 0; };
+
+	int GetDataCount() const { return dataCount; }
+	int GetEfficiency() const { return efficiency; }
+	void ClearEfficiency() { efficiency = 0; }
+
+	bool IsEmpty() const { return dataCount == 0; }
+	virtual bool IsFull() const = 0;
+
+	virtual bool Find(TKey) = 0;                           //Поиск записи в таблице по ключу
+	virtual bool Insert(TRecord record) = 0;               //Вставка записи в таблицу
+	virtual bool Delete(TKey key) = 0;                     //Удаление записиси из таблицы
+
 	virtual void GoNext() = 0;
+	virtual void Reset() = 0;
 	virtual bool IsEnd() = 0;
-	virtual TKey GetKey() { } // вернуть текующую запись 
+
+	virtual TRecord GetCurrentRecord() = 0;                //Возвращает текущую запись с ключём
+	virtual void SetCurrentRecord(TRecord record) = 0;     //Устанавливает текущую запись с ключём????
 
 };
 
